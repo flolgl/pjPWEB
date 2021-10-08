0501c3fff4dc50185f5642761d0564f23ded3111
Flo <florianlegal@outlook.fr>
2021-10-08 19:26:56 +0200

    Panier lié à la session

--- controle/Cart.php
+++ controle/Cart.php
@@ -3,64 +3,43 @@
 class Cart
 {
     public function renderCart(){
+
         $articles = array();
-        $articles["nb"] = 0;
-        $articles["totalPrix"] = 0;
-        $articles["products"] = array();
-        $articles["products"][] = array();
+        $prixTotal = 0;
 
+        if (!isset($_SESSION["panier"])){
+            require("./vue/cart.html");
+            return;
+        }
 
-        $articles["products"][0]["prix"] = 5;
-        $articles["products"][0]["brand"] = "Peugeot";
-        $articles["products"][0]["model"] = "508";
-        $articles["products"][0]["nomCateg"] = "Voiture prestige";
-        $articles["products"][0]["color"] = "Bleue";
-        $articles["products"][0]["nbPlaces"] = 5;
-
-
-        $articles["products"][1]["prix"] = 500;
-        $articles["products"][1]["brand"] = "Peugeot";
-        $articles["products"][1]["model"] = "208";
-        $articles["products"][1]["nomCateg"] = "Voiture normale";
-        $articles["products"][1]["color"] = "Bleue";
-        $articles["products"][1]["nbPlaces"] = 3;
-
-
-        $articles["products"][2]["prix"] = 5;
-        $articles["products"][2]["brand"] = "Peugeot";
-        $articles["products"][2]["model"] = "208";
-        $articles["products"][2]["nomCateg"] = "Voiture prestige";
-        $articles["products"][2]["color"] = "Bleue";
-        $articles["products"][2]["nbPlaces"] = 3;
-
-
-        $articles["products"][3]["prix"] = 5;
-        $articles["products"][3]["brand"] = "Peugeot";
-        $articles["products"][3]["model"] = "508";
-        $articles["products"][3]["nomCateg"] = "Voiture prestige";
-        $articles["products"][3]["color"] = "Bleue";
-        $articles["products"][3]["nbPlaces"] = 3;
-
-
-        $articles["products"][4]["prix"] = 5;
-        $articles["products"][4]["brand"] = "Peugeot";
-        $articles["products"][4]["model"] = "508";
-        $articles["products"][4]["nomCateg"] = "Voiture prestige";
-        $articles["products"][4]["color"] = "Bleue";
-        $articles["products"][4]["nbPlaces"] = 3;
-
-
-        $articles["products"][5]["prix"] = 5;
-        $articles["products"][5]["brand"] = "Peugeot";
-        $articles["products"][5]["model"] = "508";
-        $articles["products"][5]["nomCateg"] = "Voiture prestige";
-        $articles["products"][5]["color"] = "Bleue";
-        $articles["products"][5]["nbPlaces"] = 3;
-
-
+        require("./modele/VoitureDB.php");
+        for ($i = 0; $i<count($_SESSION["panier"]); $i++){
+            if (!isset($_SESSION["panier"][$i]))
+                continue;
+            $car = VoitureDB::getVoitureFromId($_SESSION["panier"][$i]);
+            $articles[$i]["model"] = $car[0]["type"];
+            $articles[$i]["id"] = $car[0]["id"];
+            $articles[$i]["prix"] = $car[0]["prix"];
+            $articles[$i]["photo"] = $car[0]["photo"];
+            $articles[$i]["caract"] = json_decode($car[0]["caract"], true);
+            $prixTotal = $car[0]["prix"];
+        }
 
 
         require("./vue/cart.html");
+
+    }
+
+    public function removeCarFromPanier(){
+
+        for ($i = 0; $i<count($_SESSION["panier"]); $i++){
+            if ($_GET["voitureId"] == $_SESSION["panier"][$i]) {
+                $_SESSION["panier"][$i] = null;
+                break;
+            }
+
+        }
+        header("Location: ./index.php?controle=Cart&action=renderCart");
 
     }
 }
